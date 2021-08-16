d2644e81679f9fcd22cfd56531bf4dc0348126eb
jinchoi96 <babjinny@naver.com>
2021-08-16 10:34:14 +0900

    remove previous

--- run_video.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-workload_instance=$1
-
-MALI_INSTANCE=$workload_instance /opt/rse-demo/rse-compositor &
-MALI_INSTANCE=$workload_instance /opt/rse-demo/rse-compositor &
-MALI_INSTANCE=$workload_instance /opt/rse-demo/rse-compositor &
-MALI_INSTANCE=$workload_instance /opt/rse-demo/rse-compositor &
-MALI_INSTANCE=$workload_instance /opt/rse-demo/rse-compositor &
